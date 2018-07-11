b894e12f2890655c01b2519016f755f81a76bc1f
Anton Punith <antonpunith@gmail.com>
2018-07-12 06:05:32 +1000

    configuration for Okta OAuth

--- App.js
+++ App.js
@@ -1,23 +1,61 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
+import TokenClient from '@okta/okta-react-native';
+
+const tokenClient = new TokenClient({
+  issuer: 'https://dev-635406.oktapreview.com/oauth2/default',
+  client_id: '0oafpooaszOm1rHy20h7',
+  scope: 'openid profile',
+  redirect_uri: __DEV__ ?
+    'exp://localhost:19000/+expo-auth-session' :
+    'com.oktapreview.dev-635406:/+expo-auth-session'
+});
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { authenticated: false };
+    this.checkAuthentication = this.checkAuthentication.bind(this);
+    this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
+  }
+
+  async componentDidMount() {
+    await this.checkAuthentication();
+  }
+
+  async checkAuthentication() {
+    const authenticated = await tokenClient.isAuthenticated();
+    if (authenticated !== this.state.authenticated) {
+      this.setState({ authenticated });
+    }
+  }
+
+  async login() {
+    await tokenClient.signInWithRedirect();
+    this.checkAuthentication();
+  }
+
+  async logout() {
+    await tokenClient.signOut();
+    this.checkAuthentication();
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={{ paddingTop: 100 }}>
+        <Text>Okta + React Native</Text>
+        {this.state.authenticated ?
+          <Button
+            onPress={async () => { this.logout(); }}
+            title="Logout"
+          /> :
+          <Button
+            onPress={async () => { this.login(); }}
+            title="Login"
+          />
+        }
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
